e38cb7ccf2048dc9f6d47fecbc9bdd85dddcde20
Gregory Jefferis <jefferis@gmail.com>
2018-07-09 16:47:01 +0100

    nblast clustering of LPTCIN / comparison HS inputs

---
+++ HS-LPCTIN-Connections.R
@@ -0,0 +1,63 @@
+# code comparing input numbers onto HS cells
+# and input position
+# assumes NBLAST-Clustering-LPTCIN.R has already been run
+
+hsrout=catmaid_get_connectors_between(pre_skids = 'HS Cell Right', post_skids = lptcin[,'skid'])
+hsrin=catmaid_get_connectors_between(post_skids = 'HS Cell Right', pre_skids = lptcin[,'skid'])
+
+hsrout$groupname="other"
+hsrout$groupname[hsrout$post_skid %in% g6]="uLPTCIN incomplete"
+hsrout$groupname[hsrout$post_skid %in% g5]="uLPTCIN complete"
+
+hsrout$group=lptcin.hcr.groups[match(hsrout$post_skid, names(lptcin.hcr.groups))]
+
+table(hsrout$post_skid,hsrout$pre_skid)
+
+table(hsrin$pre_skid, hsrin$post_skid)
+hscellnames=sub(".*(HS[NES]).*","\\1",catmaid_get_neuronnames('HS Cell Right'))
+
+hsrout$hscell=hscellnames[match(hsrout$pre_skid,names(hscellnames))]
+table(hsrout$post_skid,hsrout$hscell)
+hsrin$hscell=hscellnames[match(hsrin$post_skid,names(hscellnames))]
+table(hsrin$pre_skid, hsrin$hscell)
+hsrin$group=lptcin.hcr.groups[match(hsrin$pre_skid, names(lptcin.hcr.groups))]
+table(hsrin$group, hsrin$hscell)
+groupnames=c(`1`='funny', '2'='ring', '3'='ring incomplete', '4'='bilat', '5'='uni', '6'='uni incomplete')
+hsrin$groupname=groupnames[match(hsrin$group, names(groupnames))]
+table(hsrin$groupname, hsrin$hscell)
+
+
+head(hsrin)
+
+hssin=subset(hsrin, hscell=='HSS')
+points3d(hssin[,c("post_node_x","post_node_y","post_node_z")])
+
+hscells=read.neurons.catmaid('HS Cell Right')
+hscells[,'hscell']=hscellnames
+plot3d(hscells, hscell=='HSS')
+clear3d()
+
+plot3d(hscells, hscell=='HSS')
+spheres3d(subset(hssin[,c("post_node_x","post_node_y","post_node_z")], hssin[,'groupname']=="bilat"), radius=500)
+spheres3d(subset(hssin[,c("post_node_x","post_node_y","post_node_z")], grepl("uni", hssin[,'groupname'])), col='blue', radius=500)
+spheres3d(subset(hssin[,c("post_node_x","post_node_y","post_node_z")], hssin[,'groupname']%in%c("ring", "funny", "ring incomplete")), col='cyan', radius=500)
+
+
+hsrin.all=catmaid_get_connectors_between(post_skids = 'HS Cell Right')
+hsrin.all.nolptc=subset(hsrin.all, !pre_skid%in%lptcin[,'skid'])
+nopen3d()
+spheres3d(subset(hsrin.all.nolptc, post_skid==4058824)[,c("post_node_x","post_node_y","post_node_z")], col='grey', , radius=500)
+
+
+plot_hs_inputs <- function(cell=c("HSN", "HSE", "HSS")) {
+  hsin=subset(hsrin, hscell%in%cell)
+  xyz=c("post_node_x","post_node_y","post_node_z")
+
+  spheres3d(subset(hsin[,xyz], hsin[,'groupname']=="bilat"), radius=500)
+  spheres3d(subset(hsin[,xyz], grepl("uni", hsin[,'groupname'])), col='blue', radius=500)
+  spheres3d(subset(hsin[,xyz], hsin[,'groupname']%in%c("ring", "funny", "ring incomplete")), col='cyan', radius=500)
+  hscellnames <- c(`827034` = "HSE", `830793` = "HSN", `4058824` = "HSS")
+  sel=hscellnames[hscellnames%in%cell]
+  spheres3d(subset(hsrin.all.nolptc, post_skid%in%names(sel))[,xyz], col='grey', radius=500)
+  plot3d(names(sel), db=hscells)
+}
---
+++ NBLAST-Clustering-LPTCIN.R
@@ -0,0 +1,86 @@
+library(elmr)
+
+al=catmaid_get_annotationlist()
+types=c("rLPTCIN", "uLPTCIN", "bLPTCIN")
+
+# fetch all the neurons
+lptcin=read.neurons.catmaid('LPTCIN')
+regtemplate(lptcin)=FAFB
+ongoing=catmaid_skids("Chiappe-Ongoing")
+
+# nb as.character because skids are numeric
+lptcin=setdiff(lptcin, as.character(ongoing))
+
+lptcin[,'side']='R'
+
+rside='RIGHT HEMISPHERE'
+lside='LEFT HEMISPHERE'
+
+lskids=as.character(intersect(names(lptcin), catmaid_skids(lside)))
+lptcin[lskids,'side']='L'
+
+nopen3d()
+plot3d(lptcin, soma=T, col=side)
+plot3d(FAFB)
+find.neuron(db = lptcin)
+
+lptcin[,'label']=sub("LPTCIN.*","LPTCIN", lptcin[,'name'])
+
+## Cluster those neurons with NBLAST
+
+lptcin.dps=dotprops(lptcin/1000, resample=1, k=5)
+lptcin.aba=nblast_allbyall(lptcin.dps)
+lptcin.hc=nhclust(scoremat = lptcin.aba)
+
+plot(lptcin.hc, labels=lptcin[,'label'])
+plot3d(lptcin.hc, db=lptcin, k=4, soma=T)
+clear3d()
+
+library(dendroextras)
+lptcin.dend=colour_clusters(lptcin.hc, k=4)
+
+# fetch labels in dendrogram order and then set them
+labels(lptcin.dend) <- lptcin[labels(lptcin.dend),'label']
+par(mar=c(8,2,2,2))
+plot(lptcin.dend)
+
+# Let's mirror these neurons
+
+lptcin.jfrc2013 <- xform_brain(lptcin, reference = JFRC2013)
+lptcin.jfrc2013.m <- mirror_brain(lptcin.jfrc2013, subset=lptcin[,"side"]=="L")
+clear3d()
+plot3d(lptcin.jfrc2013.m, col=side)
+
+# let's cluster
+lptcin.jfrc2013.dps=dotprops(lptcin.jfrc2013.m, resample=1, k=5)
+lptcin.jfrc2013.aba=nblast_allbyall(lptcin.jfrc2013.dps)
+lptcin.jfrc2013.hc=nhclust(scoremat = lptcin.jfrc2013.aba)
+plot(lptcin.jfrc2013.hc,labels=lptcin[,'label'])
+
+# Interim conclusion is that LHS and RHS are too different after mapping
+
+rids=subset(lptcin, side=='R', rval='name')
+lptcin.hcr=nhclust(neuron_names = rids, scoremat = lptcin.aba)
+
+
+plot(lptcin.hcr, labels = lptcin[rids,'label'])
+abline(h=.65) # gives 6 groups
+lptcin.dr=as.dendrogram(lptcin.hcr)
+plot(lptcin.dr)
+
+nopen3d()
+plot3d(lptcin.hcr, k=6, groups = 1:3, db=lptcin)
+
+# nb dendroextras::slice gives groups numbered in dendrogram order
+# unlike cutree which gives groups numbered by the original sort order of the
+# first neuron in the group
+lptcin.hcr.groups=dendroextras::slice(lptcin.hcr, k=6)
+g5=subset(lptcin.hcr, groups=5, k=6)
+g6=subset(lptcin.hcr, groups=6, k=6)
+
+lptcin[,'group']=lptcin.hcr.groups[match(lptcin[,'skid'], names(lptcin.hcr.groups))]
+
+
+# this group contains incomplete neurons
+catmaid_set_annotations_for_skeletons(subset(lptcin.hcr, k=6, groups=3), "ME_TRACE_MORE")
+
