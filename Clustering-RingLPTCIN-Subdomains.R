8c4359a04f12e6ab80e0995c10036fba733a95d5
Mert Erginkaya <wxmert@gmail.com>
2018-08-02 15:20:08 +0100

    Cropping dorsal and ventral branches of ring LPTCINs with a single cut to conserve the correct node IDs
    All-by-all NBLAST clustering based on differences in the cropped branches

--- Clustering-RingLPTCIN-Subdomains.R
+++ Clustering-RingLPTCIN-Subdomains.R
@@ -13,17 +13,41 @@ regtemplate(lptcin)=FAFB
 lptcin[,'label']=sub("LPTCIN.*","LPTCIN", lptcin[,'name'])
 
 # Get all the RHS ring LPTCINs
-right.rings=subset(lptcin, label=="Right rLPTCIN", rval='name')
-right.rings
+right.rings=subset(lptcin, label=="Right rLPTCIN")
+right.rings[,]
 # Take one of the ring neurons and excise a piece of it flanked by two tags
 
-ringo = read.neuron.catmaid((right.rings[1]))
+ringo = right.rings[[1]]
 
-ringo.cutone=subset(ringo,distal_to(ringo,node.pointno=ringo$tags$`upper bound end`),invert = T)
-ringo.cuttwo=subset(ringo.cutone,distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`))
+extract_ring_subdomain <- function(ringo) {
+  ringo.cutone=subset(ringo,distal_to(ringo,node.pointno=ringo$tags$`upper bound end`),invert = T)
+  ringo.cuttwo=subset(ringo.cutone,distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`))
+  ringo.cuttwo.nodes=distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`)
+  ringo.cuttwo.nodeids=ringo.cuttwo$d$PointNo
 
-ringo.cutonebot=subset(ringo,distal_to(ringo,node.pointno=ringo$tags$`lower bound end`),invert = T)
-ringo.cuttwobot=subset(ringo.cutonebot,distal_to(ringo.cutonebot,node.pointno=ringo$tags$`lower bound start`))
+  ringo.cutone.downstream=distal_to(ringo,node.pointno=ringo$tags$`upper bound end`)
+  ringo.cuttwo.downstream=distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`)
+  ringo.top.tokeep=setdiff(ringo.cuttwo.downstream, ringo.cutone.downstream)
+
+
+  ringo.cutonebot=subset(ringo,distal_to(ringo,node.pointno=ringo$tags$`lower bound end`),invert = T)
+  ringo.cuttwobot.nodes=distal_to(ringo.cutonebot,node.pointno=ringo$tags$`lower bound start`)
+  ringo.cuttwobot=subset(ringo.cutonebot,distal_to(ringo.cutonebot,node.pointno=ringo$tags$`lower bound start`))
+  ringo.cuttwobot.nodeids=ringo.cuttwobot$d$PointNo
+
+  # now use the nodes from the top and bottom to define a single subset
+  # first though, we need to translate the indices from e.g. ringo.cutone back to
+  ringo.both.nodeids=c(ringo.cuttwo.nodeids, ringo.cuttwobot.nodeids)
+  # now subset wants node indices not unique ids
+  node.indices=match(ringo.both.nodeids, ringo$d$PointNo)
+  ringo.both=subset(ringo, node.indices)
+  ringo.both
+}
+
+ringo1.cut=extract_ring_subdomain(right.rings[[1]])
+
+
+ring.subdomains=nlapply(right.rings, extract_ring_subdomain)
 
 # Visualize the excised regions
 nopen3d()
