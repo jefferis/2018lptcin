47641b69d0f2552d42325a75ef6288f259eaa154
Mert Erginkaya <wxmert@gmail.com>
2018-08-07 11:03:05 +0100

    Commented out unused/optional code

--- Clustering-RingLPTCIN-Subdomains.R
+++ Clustering-RingLPTCIN-Subdomains.R
@@ -22,16 +22,16 @@ ringo = right.rings[[1]]
 extract_ring_subdomain <- function(ringo) {
   ringo.cutone=subset(ringo,distal_to(ringo,node.pointno=ringo$tags$`upper bound end`),invert = T)
   ringo.cuttwo=subset(ringo.cutone,distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`))
-  ringo.cuttwo.nodes=distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`)
+  # ringo.cuttwo.nodes=distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`)
   ringo.cuttwo.nodeids=ringo.cuttwo$d$PointNo
 
-  ringo.cutone.downstream=distal_to(ringo,node.pointno=ringo$tags$`upper bound end`)
-  ringo.cuttwo.downstream=distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`)
-  ringo.top.tokeep=setdiff(ringo.cuttwo.downstream, ringo.cutone.downstream)
+  # ringo.cutone.downstream=distal_to(ringo,node.pointno=ringo$tags$`upper bound end`)
+  # ringo.cuttwo.downstream=distal_to(ringo.cutone,node.pointno=ringo$tags$`upper bound start`)
+  # ringo.top.tokeep=setdiff(ringo.cuttwo.downstream, ringo.cutone.downstream)
 
 
   ringo.cutonebot=subset(ringo,distal_to(ringo,node.pointno=ringo$tags$`lower bound end`),invert = T)
-  ringo.cuttwobot.nodes=distal_to(ringo.cutonebot,node.pointno=ringo$tags$`lower bound start`)
+  # ringo.cuttwobot.nodes=distal_to(ringo.cutonebot,node.pointno=ringo$tags$`lower bound start`)
   ringo.cuttwobot=subset(ringo.cutonebot,distal_to(ringo.cutonebot,node.pointno=ringo$tags$`lower bound start`))
   ringo.cuttwobot.nodeids=ringo.cuttwobot$d$PointNo
 
@@ -50,13 +50,21 @@ ringo1.cut=extract_ring_subdomain(right.rings[[1]])
 ring.subdomains=nlapply(right.rings, extract_ring_subdomain)
 
 # Visualize the excised regions
+#nopen3d()
+#plot3d(ringo)
+#plot3d(ringo.cuttwo)
+#plot3d(ringo.cuttwobot)
+#
+
+ring.subdomains.dps=dotprops(ring.subdomains/1e3, k=5, resample=1)
 nopen3d()
-plot3d(ringo)
-plot3d(ringo.cuttwo)
-plot3d(ringo.cuttwobot)
+plot3d(ring.subdomains.dps)
 
-#It is unclear how to apply the same process to the entire list
+ring.subdomains.aba=nblast_allbyall(ring.subdomains.dps)
+ring.subdomains.hc=nhclust(scoremat = ring.subdomains.aba)
 
-#lptcin.mids=nlapply(lptcin,subset,ringo.cuttwo, OmitFailures = T)
-#lptcin.mids=nlapply(lptcin,subset,xyzmatrix(ringo.cuttwo), OmitFailures = T)
-#lptcin.mids[,]
+plot(ring.subdomains.hc)
+
+plot(ring.subdomains.hc, labels = ring.subdomains[,"name"])
+
+
